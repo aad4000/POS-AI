ab1d71f4fb1589c76120e355c0dda5e0f90eddc2
MG <you@example.com>
2024-08-20 11:56:59 +0300

    Using selenium driver

--- amazon.py
+++ amazon.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 # Step 1: Send a GET request to the website
 url = "https://www.amazon.com/SteelSeries-Apex-RGB-Gaming-Keyboard/dp/B07ZGDPT4M/ref=sr_1_1?_encoding=UTF8&content-id=amzn1.sym.12129333-2117-4490-9c17-6d31baf0582a&dib=eyJ2IjoiMSJ9.wDXBdYZ5jTvV8Yb1JBou46dA1SjYNzcFtdtRYFj4dYThMPgXCqv-_N4kHJhaXF1KHZ-jy5a7hQOEOMRZ5EaNqm5eXBT6I5tfYbBvgC4qN-2nTraHdYsXeptePMUXDIcpT7Zi-9t1wKb38VlRhSBUdRVRDRRWdq9i9wWvaMf82o6wb8Cmt-PY840-WLOEx40ytsoxPhfT4xFvDbkPRT7imMt74dGOSDfLjEpEv7rcZnA.T7IUh7VojCd0-lbc0_Eb-vTBlNqBHZmxUhhH0YwXUi8&dib_tag=se&keywords=gaming+keyboard&pd_rd_r=cc9275f6-fe29-44ca-88a9-ed111272801e&pd_rd_w=HaBkI&pd_rd_wg=5kJZf&pf_rd_p=12129333-2117-4490-9c17-6d31baf0582a&pf_rd_r=CKN0A174M8006KB8J8DP&qid=1724142951&sr=8-1"
@@ -11,11 +12,15 @@ headers = {
     "Connection": "keep-alive",
 }
 
-response = requests.get(url, headers=headers)
-response.encoding = 'utf-8'
+# response = requests.get(url, headers=headers)
+# response.encoding = 'utf-8'
+
+driver = webdriver.Chrome()  # or any other browser driver
+driver.get(url)
+
 
 # Step 2: Parse the HTML content of the page with BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+soup = BeautifulSoup(driver.page_source, "html.parser")
 
 # Step 3: Find all article titles
 # Assuming the article titles are inside <h2> tags with a specific class
@@ -30,3 +35,5 @@ soup = BeautifulSoup(response.content, "html.parser")
 
 # Print the list of titles
 print(soup)
+
+driver.quit()
